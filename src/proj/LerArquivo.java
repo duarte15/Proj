c2c068f254e360910573ef212107c75f58eed11d
duarte15 <leladscs@hotmail.com>
2018-09-03 17:39:25 -0300

    Remoção de comentários desnecessários

--- src/proj/LerArquivo.java
+++ src/proj/LerArquivo.java
@@ -4,17 +4,9 @@ import java.util.Scanner;
 
 public class LerArquivo {
     public static void main(String[] args) {
-        //Sistema teste = new Sistema();
-        //teste.criarNovoArq();
-        //System.out.println("Inicializando");
         MyTxtFile txt = new MyTxtFile();
-        //System.out.println("objeto criado");
-        //txt.read();
-        //System.out.println("função read chamada");
         MyHTMLFile ht = new MyHTMLFile();
-        //ht.read();
         //MyPDFFile pd = neww MyPDFFile();
-        //pd.read();
         System.out.println("Informe o tipo de arquivo que você deseja importar: \n 1- Arquivo com extensão txt;\n 2- Arquivo com extensão html;\n 3- Arquivo com extensão PDF;");
         Scanner sc = new Scanner(System.in);
         int opcao = sc.nextInt();
